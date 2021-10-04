36ac5428c61562210ad37694c414278eb10581f7
sebast825 <sebamolina825@gmail.com>
2021-10-04 15:07:30 -0300

    funcionando

--- 08_doList/src/index.js
+++ 08_doList/src/index.js
@@ -1,7 +1,7 @@
 import {  addDays } from "date-fns"
 const app =()=>{
 	const date = new Date()
-	let addDays = addDays(date,1)
-	console.log(addDays)
+	let asd = addDays(date,1)
+	console.log(asd)
 }
 app()
