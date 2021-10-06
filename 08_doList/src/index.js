6ad14553a027e8768bf99bbba274245d464dc453
sebast825 <sebamolina825@gmail.com>
2021-10-06 13:08:30 -0300

    con id + verificar fecha

--- 08_doList/src/index.js
+++ 08_doList/src/index.js
@@ -7,61 +7,74 @@ const app =()=>{
 }
 app()
 
-
+// 'strict mode';
 console.log(saludar())
 let container = document.querySelector('.container');
 let almacenar=[];
 
-const Tarea = class Tarea{	
-	constructor(title, desc, dueDate,complete=false){
-		this.__uniqueid = '',
-		this._title = title,
-		this._desc = desc,
-		this._dueDate = dueDate,
-		this._complete = complete
-		
-		
-	}	
-	set title (title){
-		return this._title = title
-	}
-	set desc(desc){
-		return this._desc = desc
+let Tarea = class Tarea{
+		constructor(numId,title, desc, dueDate,complete=false){
+			
+		this.numId = numId,
+		this.title = title,
+		this.desc = desc,
+		this.dueDate = dueDate
+		this.complete = complete	
+		}
+		asd(){
+			// console.log('desde asd')
+			let b = (`${this.numId}, ${this.title},${this.desc},${this.dueDate},${this.complete}`)
+			almacenar.push(b)
+			console.log(almacenar)			
+		}	
+	
 	}		
-	set duaDate (dueDate){
-		return this._dueDate = dueDate
-	}
-	set complete(a){
-		return this._complete = !this.complete
-	}
-	get actualizarNumTarea (){
-		console.log('almacenar',almacenar[0].split(' '))
-		let numId = 0;
-		if (almacenar.length!=0){
-			let idAnterior =almacenar[-1][0];
-			numId = idAnterior+1;
-		}		
-		return this.__uniqueid = numId;
-		
-	}
-	
-	get props(){
-		let elem =[`${this.__uniqueid} ${this._title}${this._desc} ${this._dueDate}${this.num}`]
-	
-		almacenar.push(elem)
-	}
-		
+
+
+function getFormulario(e){
+	e.preventDefault()
+	//  r.split(',')	 
+	//   console.log('alamacenar',almacenar)
+	//   let convertir = JSON.stringify(a)
+	//   let pedacito = convertir.split(':')
+	//   console.log(pedacito[0])
+	 let infoName = this.nameTask.value;
+	 let infoTask = this.descTask.value;
+	 let infoDate = this.dateTask.value ? this.dateTask.value  : undefined;
+	let numId = crearId();
+	console.log(numId)
+	let elem;
+	if (infoName!=undefined){
+		elem = new Tarea(numId,infoName,infoTask,infoDate)
+		}
+		// elem.numId()
+		elem.asd()	
+	// console.log(elem)
+	// almacenar.push(elem)	 
+	// mostrarDatos()
+}
+document.formulario.addEventListener('submit',getFormulario)
+
+
+function crearId(){	
+	let numId;
+		if(almacenar.length==0){
+			numId= 0;
+		}else{
+			let lastId = almacenar[almacenar.length-1];
+			numId = parseInt(lastId.split(',')[0])+1			
+		}
+		return numId
+		// let a = almacenar[almacenar.length - 1]
+		// console.log('esto es a',a.split(',')[0])
 }
 
-// let nota = new Tarea('a','b','c','d')
-// nota.notes('asd')
-// console.log(nota)
+
+
 
 function mostrarDatos(){
 	almacenar.forEach(elem=>{
-		console.log(elem)
-		//  elem.slice(' ')
-		console.log('dividido',elem[0])
+		console.log(elem[0],elem[1])
 		let cont = document.createElement('DIV')
 		let nameTask = document.createElement('H2');
 		let descTask = document.createElement('P')
@@ -73,32 +86,3 @@ function mostrarDatos(){
 
 	})
 }
-
-// let ja = new Tarea('a');
-// console.log(ja)
-
-//lee el name del formulario
-function getFormulario(e){
-	e.preventDefault()
-	let infoName = this.nameTask.value;
-	let infoTask = this.descTask.value;
-	let infoDate = this.dateTask.value;
-
-	let elem;
-	if (infoName!=undefined){
-		elem = new Tarea()
-		elem.title=(infoName)
-	}if (infoTask!=undefined){
-		elem.desc=(infoTask)
-	}if (infoDate!=undefined){
-		elem.duaDate=(infoDate)
-	}
-	let obj = [infoName,infoTask,infoDate]
-	// console.log(elem)
-	elem.actualizarNumTarea;
-	// almacenar.push(obj)
-	 elem.props	
-	mostrarDatos()
-	console.log(almacenar)
-}
-document.formulario.addEventListener('submit',getFormulario)
